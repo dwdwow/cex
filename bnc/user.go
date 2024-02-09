afc196857fc23e2dd1119444e71db525ecfd2ae8
dwd <wenyi173@gmail.com>
2024-02-09 23:06:59 +0800

    add struct: bnc/user.User

--- bnc/user.go
+++ bnc/user.go
@@ -1,1 +1,15 @@
 package bnc
+
+import "github.com/dwdwow/cex"
+
+type User struct {
+	api cex.Api
+}
+
+func NewUser(apiKey, secretKey string) User {
+	return User{cex.Api{
+		Cex:       cex.BINANCE,
+		ApiKey:    apiKey,
+		SecretKey: secretKey,
+	}}
+}
