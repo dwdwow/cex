ff53e6cc35f3a55374c1dbc8e049f2b9ebe27a84
dwd <wenyi173@gmail.com>
2024-02-09 23:21:09 +0800

    add func: bnc/user.signReqData

--- bnc/user.go
+++ bnc/user.go
@@ -1,15 +1,44 @@
 package bnc
 
-import "github.com/dwdwow/cex"
+import (
+	"github.com/dwdwow/cex"
+	"github.com/dwdwow/s2m"
+	"net/url"
+	"strconv"
+	"time"
+)
 
 type User struct {
 	api cex.Api
 }
 
 func NewUser(apiKey, secretKey string) User {
-	return User{cex.Api{
-		Cex:       cex.BINANCE,
-		ApiKey:    apiKey,
-		SecretKey: secretKey,
-	}}
+	return User{
+		api: cex.Api{
+			Cex:       cex.BINANCE,
+			ApiKey:    apiKey,
+			SecretKey: secretKey,
+		},
+	}
+}
+
+func (u User) sign(data any) (query string, err error) {
+	return signReqData(data, u.api.SecretKey)
+}
+
+func signReqData(data any, key string) (query string, err error) {
+	val := url.Values{
+		"timestamp": []string{strconv.FormatInt(time.Now().UnixMilli(), 10)},
+	}
+	m, err := s2m.ToStrMapWithErr(data)
+	if err != nil {
+		return
+	}
+	for k, v := range m {
+		val.Set(k, v)
+	}
+	query = val.Encode()
+	sig := cex.SignByHmacSHA512ToHex(query, key)
+	query += "&signature=" + sig
+	return
 }
