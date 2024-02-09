58772254b0421d8d34ecac3d4fb71d1a2ff9c18c
dwd <wenyi173@gmail.com>
2024-02-09 23:00:29 +0800

    remove func: cex.Api.Sign

--- api_key.go
+++ api_key.go
@@ -5,13 +5,4 @@ type Api struct {
 	ApiKey     string `json:"apiKey,omitempty" bson:"apiKey"`
 	SecretKey  string `json:"secretKey,omitempty" bson:"secretKey"`
 	Passphrase string `json:"passphrase,omitempty" bson:"passphrase"`
-
-	signer Signer
-}
-
-func (api Api) Sign(payload, key string) []byte {
-	if api.signer != nil {
-		return api.signer(payload, key)
-	}
-	return nil
 }
