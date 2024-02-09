5ea81495e0faafa0f8ac36ce39ceea49c96d004e
dwd <wenyi173@gmail.com>
2024-02-09 20:14:22 +0800

    add interface: ReqHandler

--- req.go
+++ req.go
@@ -64,15 +64,15 @@ func (rc ReqConfig[ReqType, RespType]) BaseConfig() BaseConfig {
 
 type ReqOpt func(*resty.Client, *resty.Request)
 
-// ReqMaker is a func type which should be implemented by all cex.
-// Cex should provide a copy of its own resty client.
-type ReqMaker func(config BaseConfig, reqData any, opts ...ReqOpt) (*resty.Request, error)
+// ReqHandler should be implemented in all cex package
+type ReqHandler interface {
+	Make(config BaseConfig, reqData any, opts ...ReqOpt) (*resty.Request, error)
+	Check(*resty.Response, *resty.Request) error
+}
 
-type RespChecker func(*resty.Response, *resty.Request) error
-
-func Request[ReqType, RespType any](config ReqConfig[ReqType, RespType], reqData ReqType, maker ReqMaker, respChecker RespChecker, opts ...ReqOpt) (RespType, error) {
+func Request[ReqType, RespType any](config ReqConfig[ReqType, RespType], reqData ReqType, handler ReqHandler, opts ...ReqOpt) (RespType, error) {
 	respData := new(RespType)
-	req, err := maker(config.BaseConfig(), reqData, opts...)
+	req, err := handler.Make(config.BaseConfig(), reqData, opts...)
 	if err != nil {
 		return *respData, err
 	}
@@ -93,7 +93,7 @@ func Request[ReqType, RespType any](config ReqConfig[ReqType, RespType], reqData
 		return *respData, fmt.Errorf("cex: http method %v is not supported", config.Method)
 	}
 
-	if err = respChecker(resp, req); err != nil {
+	if err = handler.Check(resp, req); err != nil {
 		return *respData, fmt.Errorf("cex: check response, %w", err)
 	}
 
