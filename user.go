c5e77e2e0f9297aa8c65b35d3e824978b0d53845
dwd <wenyi173@gmail.com>
2024-02-09 20:38:34 +0800

    add struct: Api

--- user.go
+++ user.go
@@ -1,1 +1,12 @@
 package cex
+
+type Api struct {
+	ApiKey     string `json:"apiKey,omitempty" bson:"apiKey"`
+	SecretKey  string `json:"secretKey,omitempty" bson:"secretKey"`
+	Passphrase string `json:"passphrase,omitempty" bson:"passphrase"`
+}
+
+type User interface {
+	Cex() Cex
+	Api() Api
+}
