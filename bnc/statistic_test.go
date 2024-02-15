7eb89655ea74d69dd0a8f8d3d39dee0533347d67
dwd <wenyi173@gmail.com>
2024-02-15 12:29:30 +0800

    add func: bnc.TestCountFundingRateTimeSeries

--- bnc/statistic_test.go
+++ bnc/statistic_test.go
@@ -1,1 +1,24 @@
 package bnc
+
+import (
+	"testing"
+	"time"
+
+	"github.com/dwdwow/props"
+)
+
+func TestCountFundingRateTimeSeries(t *testing.T) {
+	info := FuturesFundingRateInfo{
+		Symbol:                   "ETHUSDT",
+		AdjustedFundingRateCap:   0,
+		AdjustedFundingRateFloor: 0,
+		FundingIntervalHours:     8,
+		Disclaimer:               false,
+	}
+	days := 60.0
+	interval := int64(days * 24 * 60 * 60 * 1000)
+	now := time.Now().UnixMilli()
+	res, err := CountFundingRateTimeSeries(info, now-interval, now)
+	props.PanicIfNotNil(err)
+	props.PrintlnIndent(res)
+}
