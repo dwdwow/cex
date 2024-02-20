e561f3092412dd162ebd5504ecb290c6d2fb617b
dwd <wenyi173@gmail.com>
2024-02-20 17:29:41 +0800

    add func: cex.ReqOptRetryCount

--- req_opts.go
+++ req_opts.go
@@ -1,1 +1,18 @@
 package cex
+
+import (
+	"time"
+
+	"github.com/go-resty/resty/v2"
+)
+
+func ReqOptRetryCount(count int, waitTime time.Duration) ReqOpt {
+	return func(client *resty.Client, r *resty.Request) {
+		if client == nil {
+			return
+		}
+		client.SetRetryCount(count)
+		client.SetRetryWaitTime(waitTime)
+		client.SetRetryMaxWaitTime(waitTime)
+	}
+}
