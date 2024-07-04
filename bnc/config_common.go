b429adf78676bf32a1afd41975208a7ceff2fb1a
dwd <wenyi173@gmail.com>
2024-07-04 12:30:11 +0800

    add file

--- bnc/config_common.go
+++ bnc/config_common.go
@@ -1,1 +1,50 @@
 package bnc
+
+import (
+	"net/http"
+
+	"github.com/dwdwow/cex"
+)
+
+type ListenKeyResponse struct {
+	ListenKey string `json:"listenKey"`
+}
+
+var NewListenKeyConfig = cex.ReqConfig[cex.NilReqData, ListenKeyResponse]{
+	ReqBaseConfig: cex.ReqBaseConfig{
+		BaseUrl:          "",
+		Path:             "",
+		Method:           http.MethodPost,
+		IsUserData:       true,
+		UserTimeInterval: 0,
+		IpTimeInterval:   0,
+	},
+	HTTPStatusCodeChecker: HTTPStatusCodeChecker,
+	RespBodyUnmarshaler:   cex.StdBodyUnmarshaler[ListenKeyResponse],
+}
+
+var KeepListenKeyConfig = cex.ReqConfig[cex.NilReqData, ListenKeyResponse]{
+	ReqBaseConfig: cex.ReqBaseConfig{
+		BaseUrl:          "",
+		Path:             "",
+		Method:           http.MethodPut,
+		IsUserData:       true,
+		UserTimeInterval: 0,
+		IpTimeInterval:   0,
+	},
+	HTTPStatusCodeChecker: HTTPStatusCodeChecker,
+	RespBodyUnmarshaler:   nil,
+}
+
+var DeleteListenKeyConfig = cex.ReqConfig[cex.NilReqData, ListenKeyResponse]{
+	ReqBaseConfig: cex.ReqBaseConfig{
+		BaseUrl:          "",
+		Path:             "",
+		Method:           http.MethodPut,
+		IsUserData:       true,
+		UserTimeInterval: 0,
+		IpTimeInterval:   0,
+	},
+	HTTPStatusCodeChecker: HTTPStatusCodeChecker,
+	RespBodyUnmarshaler:   nil,
+}
