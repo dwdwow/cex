cf3492bee9085f3a8bad204027c686b533e38473
dwd <wenyi173@gmail.com>
2024-07-04 12:55:17 +0800

    fix test: bnc.TestWs_newFreqToken

--- bnc/ws_ori_test.go
+++ bnc/ws_ori_test.go
@@ -8,7 +8,7 @@ import (
 func TestWs_newFreqToken(t *testing.T) {
 	dur := time.Second
 	maxreqPerDur := 5
-	ws := NewWs(WsCfg{ReqDur: dur, MaxReqPerDur: maxreqPerDur}, nil)
+	ws := NewWs(WsCfg{ReqDur: dur, MaxReqPerDur: maxreqPerDur}, nil, nil)
 	for i := 0; i < maxreqPerDur*2; i++ {
 		time.Sleep(dur / time.Duration(maxreqPerDur*2))
 		ok := ws.newReqToken()
