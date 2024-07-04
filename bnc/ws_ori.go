df24971a49ed6afb01afa883cf4d7475be40700a
dwd <wenyi173@gmail.com>
2024-07-04 09:59:37 +0800

    add file

--- bnc/ws_ori.go
+++ bnc/ws_ori.go
@@ -1,7 +1,42 @@
 package bnc
 
-import "github.com/gorilla/websocket"
+import (
+	"sync"
+	"time"
+
+	"github.com/dwdwow/props"
+	"github.com/gorilla/websocket"
+)
 
 type Ws struct {
-	conn *websocket.Conn
+	conn   *websocket.Conn
+	fanout props.Fanout[[]byte]
+
+	muxReqToken   sync.Mutex
+	reqTokenDur   time.Duration
+	maxTokenNum   int
+	crrTokenIndex int
+	latestTokens  []int64
+}
+
+func (w *Ws) newReqToken() bool {
+	w.muxReqToken.Lock()
+	defer w.muxReqToken.Unlock()
+	t := time.Now().UnixMilli()
+	withinDurNum := 0
+	for _, v := range w.latestTokens {
+		if t-v < w.reqTokenDur.Milliseconds() {
+			withinDurNum++
+		}
+	}
+	if withinDurNum >= w.maxTokenNum {
+		return false
+	}
+	i := w.crrTokenIndex + 1
+	if i >= w.maxTokenNum {
+		i -= w.maxTokenNum
+	}
+	w.latestTokens[i] = t
+	w.crrTokenIndex = i
+	return true
 }
