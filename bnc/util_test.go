10db0b481ffc3d9c54368e41fffc7e669f894e25
dwd <wenyi173@gmail.com>
2024-07-06 15:19:18 +0800

    add test: bnc.TestGetWsEvent

--- bnc/util_test.go
+++ bnc/util_test.go
@@ -1,1 +1,10 @@
 package bnc
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetWsEvent(t *testing.T) {
+	fmt.Println(getWsEvent([]byte(`{\"e\":\"balanceUpdate\",\"E\":1720250097625,\"a\":\"PEPE\",\"d\":\"-100000.00\",\"T\":1720250097624}`)))
+}
