dbcf824cf55387081476a57ec4ce22c0003804d0
dwd <wenyi173@gmail.com>
2024-07-24 15:12:43 +0800

    add var: bnc.UmFuturesWsCfg

--- bnc/ws_pub_unmarshaler.go
+++ bnc/ws_pub_unmarshaler.go
@@ -27,3 +27,26 @@ var SpotPublicWsCfg = WsCfg{
 	MaxReqPerDur:    5,
 	DataUnmarshaler: SpotWsPrivateMsgUnmarshaler,
 }
+
+func UmFuturesWsPublicMsgUnmarshaler(e WsEvent, data []byte) (any, error) {
+	switch e {
+	case WsEventAggTrade:
+		return unmarshal[WsAggTradeStream](data)
+	case WsEventMarkPriceUpdate:
+		return unmarshal[WsMarkPriceUpdate](data)
+	case WsEventKline:
+		return unmarshal[WsKlineStream](data)
+	case WsEventDepthUpdate:
+		return unmarshal[WsDepthStream](data)
+	default:
+		return nil, fmt.Errorf("bnc: unknown event %v", e)
+	}
+}
+
+var UmFuturesWsCfg = WsCfg{
+	Url:             FutureWsBaseUrl,
+	MaxStream:       1024,
+	ReqDur:          time.Second,
+	MaxReqPerDur:    5,
+	DataUnmarshaler: UmFuturesWsPublicMsgUnmarshaler,
+}
